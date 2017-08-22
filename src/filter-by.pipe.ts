834fbd0abd4eb4e2025ea8997d21b646f77a558f
Clement Perdereau <c.perdereau@okservice.fr>
2017-08-22 16:14:15 +0200

    no message

--- src/filter-by.pipe.ts
+++ src/filter-by.pipe.ts
@@ -12,14 +12,12 @@ export class FilterByPipe implements PipeTransform {
     if (!items || !filter) {
       return items;
     }
-    console.log(filter);
     let props = Object.keys(items[0]);
     let newArray = [];
     for (let i = 0; i < items.length; i++) {
       let check = 0;
       for (let j = 0; j < props.length; j++) {
         let val = items[i][props[j]];
-        console.log(val);
         if (val.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1) {
           check++;
         }

fb4e0c018631efd778fb4266e1374b9e77617181
Clement Perdereau <c.perdereau@okservice.fr>
2017-08-23 16:30:50 +0200

    no message

--- src/Pipes/filter-by.pipe.ts
+++ src/Pipes/filter-by.pipe.ts
@@ -8,25 +8,9 @@ import { Injectable, PipeTransform, Pipe } from '@angular/core';
 })
 @Injectable()
 export class FilterByPipe implements PipeTransform {
-  transform(items: any[], filter, properties: any[]): any {
+  transform(items: any[], filter): any {
     if (!items || !filter) {
       return items;
-    }
-    if (properties.length > 0) {
-      let newArray = [];
-      for (let i = 0; i < items.length; i++) {
-        let check = 0;
-        for (let j = 0; j < properties.length; j++) {
-          let val = items[i][properties[j]];
-          if (val.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1) {
-            check++;
-          }
-        }
-        if (check > 0) {
-          newArray.push(items[i]);
-        }
-      }
-      return newArray;
     }
     let props = Object.keys(items[0]);
     let newArray = [];
